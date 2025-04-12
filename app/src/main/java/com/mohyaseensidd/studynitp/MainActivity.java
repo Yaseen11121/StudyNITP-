d7b0916ff32388b090c6cef1518b591b774d6afa
MYS <mohyaseensiddiqui@gmail.com>
2025-04-12 13:49:41 +0530

    Login Actvity finished

---
+++ app/src/main/java/com/mohyaseensidd/studynitp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.mohyaseensidd.studynitp;
+
+import android.content.Intent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText emailInput, passwordInput;
+    Button loginBtn;
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            String email = emailInput.getText().toString().trim();
+            String password = passwordInput.getText().toString().trim();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+                            // Go to HomeActivity
+//                            startActivity(new Intent(this, HomePage.class));
+//                            finish();
+                        } else {
+                            Toast.makeText(this, "Login failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+        TextView forgotBtn = findViewById(R.id.forgotPassword);
+        forgotBtn.setOnClickListener(v ->{
+            Toast.makeText(this,"Contact the developer!!",Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+    public void openRegisterActivity(View view){
+        Toast.makeText(this,"Enter ur details!",Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this,RegisterPage.class);
+//        startActivity(intent);
+        finish();
+    }
+
+}
